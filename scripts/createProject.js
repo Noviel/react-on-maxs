1a0f88ad71231a661bfe890ccbe340115ecb99f1
Aleksandr Snov <snov.contact@gmail.com>
2017-12-31 22:39:24 +0700

    Upgrade webpack-features

--- scripts/createProject.js
+++ scripts/createProject.js
@@ -117,7 +117,7 @@ inquirer.prompt(questions).then(answers => {
     .then(log(preInstallMessage))
     .then(
       execute(
-        `${changeDiskCommand} cd ${projectRoot} && yarn install && yarn add webpack-features@^3.2.9 --dev`
+        `${changeDiskCommand} cd ${projectRoot} && yarn install && yarn add webpack-features@^3.2.10 --dev`
       )
     )
     .then(log(postInstallMessage))
